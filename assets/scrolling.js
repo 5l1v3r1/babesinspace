1b7967a2f5eae25d1304a65d7760698d898fedc8
Alex Nichol <unixpickle@gmail.com>
2015-12-20 04:09:21 +0000

    fixed resizing+scrolling issue
    Resizing the browser had a weird effect on the scroll offset. This was especially noticeable on mobile browsers when the URL field hid after scrolling. Now it is mostly fixed.

--- assets/scrolling.js
+++ assets/scrolling.js
@@ -1,6 +1,7 @@
 (function() {
 
   var rocket;
+  var oldSize;
 
   function updateRocketRotation() {
     var scrollOffset = window.scrollY;
@@ -11,10 +12,22 @@
     rocket.style.MozTransform = 'rotate(' + angle + 'deg)';
   }
 
+  function handleResize() {
+    if (Math.abs(window.scrollY) < 10) {
+      return 0;
+    }
+    var heightDiff = window.innerHeight - oldSize;
+    window.scrollTo(0, window.scrollY+heightDiff);
+    oldSize = window.innerHeight;
+    updateRocketRotation();
+  }
+
   window.addEventListener('load', function() {
+    oldSize = window.innerHeight;
     rocket = document.getElementById('rocket');
     updateRocketRotation();
     window.addEventListener('scroll', updateRocketRotation);
+    window.addEventListener('resize', handleResize);
   });
 
 })();
